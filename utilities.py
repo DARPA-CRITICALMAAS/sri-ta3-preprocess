56b5d9893a0c12c5a1f4cb4ac3aa2c9d95d1af25
Vasily Zadorozhnyy <vasily.zadorozhnyy@sri.com>
2023-09-19 05:33:28 +0000

    s2id_to_cellid, latlong_to_cellid, and region_of_cellids functions are added

--- utilities.py
+++ utilities.py
@@ -3,6 +3,11 @@ from rasterio.transform import from_origin
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import s2sphere as s2
+
+# computed bounds for Australia and US/Canada (Note: seems like US/Canada bounds have issues)
+bounds_australia = {'left': 112.91657, 'bottom': -54.7421128072652, 'right': 159.100865, 'top': -9.221274999999999}
+bounds_uscanada = {'left': -191.00025, 'bottom': 15.696449999999999, 'right': 179.754051886584, 'top': 90.05655}
 
 def load_dataset(filename='data/2021_Table04_Datacube.csv', encoding_type='latin-1', index_col=None):
     print(f"Loading {filename}")
@@ -83,3 +88,30 @@ def determine_parameters(df, kmpdeg_lat0=111.1, crs = 'EPSG:4326', kminpix=2.563
         'crs' : crs,
     }
     return output_dict
+
+def s2id_to_cellid(id):
+    # input: id - must be an integer
+    # ouput: S2 CellId, where level is automatically determined
+    return s2.CellId(id)
+
+def latlong_to_cellid(lat, lon, s2_level):
+    # takes (lat,lon) point, and finds the CellId w/ respect to s2_level
+    point = s2.LatLng.from_degrees(lat, lon)
+    cellid = s2.CellId.from_lat_lng(point).parent(s2_level)
+    return cellid
+
+def region_of_cellids(dict_bounds, s2_level):
+    # takes bounds_australia (WORKS) or bounds_uscanada (ISSUES)
+    # outputs a list of all CellIds that cover the region
+    coverer = s2.RegionCoverer()
+    coverer.min_level = coverer.max_level = s2_level
+
+    # order matters, particularly for the s2.LatLngRect() function
+    min_lat_lng = s2.LatLng.from_degrees(dict_bounds['bottom'], dict_bounds['left']) # bottom left corner
+    max_lat_lng = s2.LatLng.from_degrees(dict_bounds['top'], dict_bounds['right']) # top right corner
+
+    # Create an S2LatLngRect from the boundary points
+    rect = s2.LatLngRect(min_lat_lng, max_lat_lng)
+
+    cell_ids = coverer.get_covering(rect)
+    return cell_ids
